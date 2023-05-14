5fce5109c9d9b656cc1835d0b75ee811d0105811
cyyoun <cyyoun41@gmail.com>
2023-05-14 13:56:26 +0900

    ch2_API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -15,8 +16,37 @@ public class HelloController {
     }
 
     @GetMapping("hello-mvc")
+    /** MVC 템플릿 엔진 **/
     public String helloMvc(@RequestParam("name") String name, Model model) { //웹에서 매개변수 받아오기
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http에서의 body 부분에 데이터를 직접 넣어주겠다는 의미의 애너테이션
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+
+    @GetMapping("hello-api") /** API **/
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello(); //String 값이 아니라 객체인 경우 Default 로 api 방식으로 json 넘겨줌
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
+ 
