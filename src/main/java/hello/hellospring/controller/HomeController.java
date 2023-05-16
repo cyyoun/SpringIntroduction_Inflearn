bf72897e87f188a2acc69b561c7a348efe6a6ad8
cyyoun <cyyoun41@gmail.com>
2023-05-16 23:43:52 +0900

    ch5_회원웹기능_등록

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -1,7 +1,9 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -9,6 +11,9 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home"; //home.html 이 없으면 index.html 을 화면에 띄워줌 (우선순위 있음)
-
     }
+
+
+
+
 }
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,21 @@ public class MemberController {
     @Autowired //생성자에 Autowired가 되어 있으면 스프링이 memberService를 알아서 맵핑해줌
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new") //GetMapping 조회할 때 사용
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new") //PostMapping 데이터를 전달할 때 사용
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
